d1eaf77153e0c30ef089a1d5587ab211eb304112
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2015-09-18 01:34:26 +0000

    eleborated on docstring

--- task_02.py
+++ task_02.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module does some pretty crazy math."""
+"""
+
+This module calls the hamlet.crazy_math() math function from hamlet.py assigning
+it with the following parameters in order.
+
+"""
 
 
 import hamlet
 
 POSITIONAL = hamlet.crazy_math(4,100000,8,98)
+
