eef51befe008eb767f36fb2aaed8d3597bde49e0
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2015-09-18 02:34:01 +0000

    added function with keywords

--- task_03.py
+++ task_03.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module does some pretty crazy math."""
+"""
 
+This module calls the hamlet.crazy_math() math function from hamlet.py assigning
+it with the following parameters that are releative to the variable in hamlet.
+
+"""
 
 import hamlet
+
+KEYWORD = hamlet.crazy_math(bananas=48,monkeys=84,hours=200000)
