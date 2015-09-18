64e88fbecc64d56f4f48cbdbe17e18a621845073
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2015-09-18 00:36:48 +0000

    added function docstring

--- task_01.py
+++ task_01.py
@@ -4,6 +4,24 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """This function knows what you mean and provides a statement.
+
+    Args:
+        wink (str): The person's name you are winking to.
+        numwink (int, optional): Number of winks and nudges returned. Default: 2
+
+    Returns:
+        str: All agruments restringed into a sentence.
+
+    Examples:
+
+        >>> know_what_i_mean('Niko')
+        'Know what I mean? NikoNiko, nudge nudge'
+    `
+        >>> know_what_i_mean('CUNY', 4)
+        'Know what I mean? CUNYCUNYCUNYCUNY, nudge nudge nudge nudge'
+        
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
